a060b467db67e7fa09dc3a31e043a7b72737df47
Gowthamraju <gowtham3897@gmail.com>
2020-12-18 16:57:01 +0530

    Angular UI

---
+++ Spring Backend/Policy_User_Details/Policy_User_Details/src/main/java/com/policy/org/Policy_User_Details/PolicyUserDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.policy.org.Policy_User_Details;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyUserDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyUserDetailsApplication.class, args);
+	}
+
+}
---
+++ Spring Backend/Policy_User_Details/Policy_User_Details/src/test/java/com/policy/org/Policy_User_Details/PolicyUserDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.policy.org.Policy_User_Details;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyUserDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
